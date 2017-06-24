61b8a1b96be14a8c20aced9d47f06cf1d2aef4ff
Hy369 <aoba_c@126.com>
2017-06-24 23:26:10 +0800

    Bug fixed.

--- src/MiniUtils/Str.php
+++ src/MiniUtils/Str.php
@@ -60,7 +60,7 @@ class Str
      */
     public static function unicodeToUtf8($str, $prefix = '\\u', $suffix = '')
     {
-        $pattern = sprintf('~%s(%s)%s~', addslashes($prefix), '[A-Fa-f\d]+', addslashes($suffix));
+        $pattern = sprintf('~%s(%s)%s~', addslashes($prefix), '[A-Fa-f\d]{1,4}', addslashes($suffix));
         $str = preg_replace_callback($pattern, function ($item) use ($prefix) {
             $bin = base_convert($item[1], $prefix === '&#' ? 10 : 16, 2);
             $group = ceil(strlen($bin) / 6);
